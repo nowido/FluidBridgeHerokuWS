9976ff2925d861bcd970d4873c80accab46816ab
nowido <nowido@mail.ru>
2018-01-14 00:50:08 +0300

    Update main_webhooks_ws.js

--- main_webhooks_ws.js
+++ main_webhooks_ws.js
@@ -169,16 +169,6 @@ setInterval(() => {
 
 const serviceAccount = 
 {
-    "type": "service_account",
-    "project_id": "fluidsync-8f7e4",
-    "private_key_id": "0a4396c204bf1f42c8329363ad9486b2916a161f",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDADGqSJtfMTYjC\nQQ8qYupDk5oT1zV2cN+47QbaqlGpTU8p6DJcBBuDb0IYc+BHEusF0ONfUtbGbYTh\nEPh0CFerosu579Say2EM0bmjTaIOM66RmxIB2RFVbIQCOX8aKFgDx+Xx2KJKzN8m\nqrm96YwIiOM2CYGmsQmOaOpOSCiU4coURTP+t6jJSsCZrpYcDw9CTPR2XMpMtS9N\nE764l6MmYyg0NfHpu4Cal7I/uMgUY+1wSYBGw62HjLotIWBkvL2gwrjLI3mkWAcW\nXU2Ke/duPOVyVv88py947XYKAMfQx7VUBRVQ83CPg/psiz4TN2X9tZrCwQXfqOL0\nQr2US+wjAgMBAAECggEAGs6g4Z3CaaY4/MSQFV6RTLWBwFs9/zZqRCnx1vwHQhak\ngjkDLoz0OjiBhWBHYiRl4f3yfw/VIwWOkswHokq30iF7Ro0PpDvXZC6yNvVTNaBJ\nMdHvhYqDPOVU1+qmr0QKZRy2IDgZD0/hpkgWfUfoYBibfqN9EYOMzfAfJKY14/IS\nfWi+fuk3gKa414SNGYohVR8yv1tNU2A4nEh88VS89r8zNgXP5dsAdwrve9EZ3lKi\nqqo+/8C2bj41149AG2Uf0mE/SUdLdMbOa3V+p+x+tGpLo+Q9MS/NQAqwGnTcJYl5\nsDoQBhIMIhNQt1L0xDbO1zi1+oOBj9DBTTCS7r3jwQKBgQDrB4C1+D8J17k1HTP/\n9jD7v5uWp3mOacgdwv4+lUEFh8wN2ZMSqVGQ1tlnxohOC0MV5r+KLyeIb9AdVv2F\n00ZV1/okebLa31wH62rn3W/SzQFGFdcISKxpNAtfeXXxVs//SqNQFu4xiNEL5qXY\nBGYPnMxNqnGKFaceUKcr9r5RKwKBgQDRLydV2XsEFRotns32WvJ69hXHEcTEmTpq\n2d1GGo9qXeLUW/Npuh7DC2ZZggQaO9JYrRv+GIN1KCxvc9Qp+C+u4NHveeOPGKZj\nLRN4qmJ8I5yO9RLLtJhs8px1kS/fQJxHuyUX8cNdlN/aMEX/b1HBcgzdEVQuy58C\nJmjbn/4k6QKBgQCkvkOYSgRV7+KgunGir30yWeLDvWzVSIy/X9k7IP/M0p1T7/jJ\nkencrt0BgwY0Pmytw2rrf5YkasvkZP+ceLXKonNycePOy9YErx3mnS8vAggsik6K\nPfasX5DBQbBMlN+DQVwsXYZlLlPqFaiWPK8VroD+x8SCWB8mfr5PcMuCtQKBgQCn\n9IBUwcWvxXG7Z2Jfhy5h4+wgZffjElSBLmQnwXOdJ7zUXO/X2ASS/jgzbdz1Y0rE\nwFlVb0E9dAAZjJqxCADhHMnyyZ8YaeZOgwa7P7LLkZxGfCeXP22TLEjLbMNPMYVJ\nYs2pqAgAv2PswA2zxiGSfIwvg5Zf7EXDaCuRC6j24QKBgQCuRXgm1a+pTDbXM+gO\nxsuROqroBqEyBN6nm6E8fEJ2y71I6NQHqaFRT/9hPeg6L4ju7JkhcsSM9OA64hJi\nvtUJMgI5st6FaY1NHwoT2scUZ/gbE2/qarj4u20s2M48RnQcCEVlWK75hRgal6ju\nfkUPk/+5v3shh16x4nl8GBMfXQ==\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-4c48v@fluidsync-8f7e4.iam.gserviceaccount.com",
-    "client_id": "117348076685795189014",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://accounts.google.com/o/oauth2/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4c48v%40fluidsync-8f7e4.iam.gserviceaccount.com"      
 };
 
 admin.initializeApp({credential: admin.credential.cert(serviceAccount)});
